6ab652dd66babf40fcda47ba700c78846af98be3
mivy <grperlot@gmail.com>
2021-10-31 21:39:37 -0700

    Update Cerulli.py

--- Cerulli.py
+++ Cerulli.py
@@ -9,14 +9,14 @@ from numpy.random.mtrand import f
 
 
 ce_constant = {
-    'ngc' : float(6.67408e-11),       
+    'ngc' : float(6.674080000e-11),      # 6.67408e-11  
     'e' : 100,                          #
     'frame_interval' : 1,               #
-    'frame_total' : 200,                 #
+    'frame_total' : 100,                 #
     'time' : 1,                         #
-    'processes_nodes' : 30,             #
-    'samples' : 100,                   #
-    }
+    'processes_nodes' : 40,             # 4, 40
+    'samples' : 250,                    # 250, 250
+    }                        # samples: # 1_000, 10_000
 aim = {}
 
 
@@ -31,7 +31,7 @@ def loopContainer(queue, i):
         set_name = f'n_{set_list_index}'
         set_type = iType(initialize_kmass, initialize_random)
         set_mass = iMass(set_type, initialize_kmass)
-        set_visual_radius = initialize_kmass * .001 # object volume (see: bpy)
+        set_visual_radius = initialize_kmass * .01 # object volume (see: bpy)
         set_location = [initialize_normal[0], initialize_normal[1], initialize_normal[2]]
         set_velocity = [0,0,0]
         initial_set = [set_frame, set_name, set_type, set_mass, set_visual_radius, set_location, set_velocity]
@@ -43,7 +43,7 @@ def loopContainer(queue, i):
 def iType(i, ii): # assign types
     if i > 3: #
         m = 'unsigned'
-    elif ii <= 15:
+    elif ii <= 80: # 15 universally
         m = 'positive'
     else:
         m = 'negative'
@@ -55,11 +55,14 @@ def iMass(i, ii): # assign mass
         ii = (ii + 1) * ((ii + 4) / 2) # about 10 sm
     elif i == 'negative':
         ii = ii * (-1) # reductive
+    ii = ii * 100000
     return ii
 
 
 def coreFunction(shared_list, history, core, frame):
     #aim = queue.get()
+    #print(history[3])
+
     for focus in range(core[0], core[1]): # total indicies counted here
         focus_object = history[focus] # and call its values
         acceleration_vector = [0,0,0] # formatting (check function)
@@ -72,10 +75,16 @@ def coreFunction(shared_list, history, core, frame):
 
             # add additional interactions here
         velocity_vector = focus_object[6]
-        velocity_delta = nDeltaTime(acceleration_vector, velocity_vector)
         location = focus_object[5]
-        location_delta = nLocationUpdate(location, velocity_delta)
+        
+        #print(velocity_vector[2]) # working
 
+        velocity_delta = nDeltaTime(acceleration_vector, velocity_vector)
+        
+        location_delta = nLocationUpdate(location, velocity_delta)
+        
+    
+        
 
         set_frame = frame
         set_name = focus_object[1]
@@ -95,25 +104,25 @@ def nQueue(i, ii): # assign iterations
 
 
 def nPythagorean(f, t):
-    p_distance = math.sqrt(abs(t[5][0] - f[5][0])**2 + abs(t[5][1] - f[5][1]**2 + abs(t[5][2] - f[5][2])**2))
+    p_distance = math.sqrt(((t[5][0] - f[5][0])**2) + ((t[5][1] - f[5][1])**2) + ((t[5][2] - f[5][2])**2))
     return p_distance
 
 
 def nSumVector(f, t, ret_distance, acceleration_vector_sum):
     acceleration_vector = [0,0,0]
-    for i in range(3): # where range is dimensions of gravity
-        newton = ((ce_constant['ngc']) * (t[3]) / (ret_distance**3) * (f[5][i] - t[5][i]))
+    for i in range(3): # where range is dimensions of gravity # ret_distance**3
+        newton = (-1) * ((ce_constant['ngc']) * (t[3]) * (f[5][i] - t[5][i])) / (ret_distance**3) # VARIABLE E ...  + (.0000000000000002**2))**(3/2)
         acceleration_vector[i] = newton
-    acceleration_vector_sum = [acceleration_vector[0] + acceleration_vector_sum[0], 
+    new_vector = [acceleration_vector[0] + acceleration_vector_sum[0], 
     acceleration_vector[1] + acceleration_vector_sum[1], 
     acceleration_vector[2] + acceleration_vector_sum[2]]
-    return acceleration_vector_sum
+    return new_vector
 
 
 def nDeltaTime(acceleration_vector, velocity_vector):
     delta_v = [0,0,0]
     for i in range(3):
-        delta_v[i] = ce_constant['time'] * acceleration_vector[i] + velocity_vector[i]
+        delta_v[i] =  ((ce_constant['time']**2) * acceleration_vector[i]) + (velocity_vector[i] * ce_constant['time'])
     return delta_v
 
 
@@ -133,9 +142,9 @@ def nLocationUpdate(location, velocity):
 if __name__ == "__main__":
     queue = mp.Manager().Queue()
     ret = {}
-    history = []
     export_data_set = []
-    
+    updated_list_of_values = []
+
     processes_n = []
     queue.put(ret)
     for i in range(ce_constant['processes_nodes']):
@@ -145,8 +154,8 @@ if __name__ == "__main__":
     for p in processes_n:
         p.join()
     ret = queue.get()
-    history = list(ret.values())
-    print(history)
+    updated_list_of_values = list(ret.values())
+    #print(history)
     #ret = list(ret.values()) # fixed when 'ret' is a list, otherwise convert the dict into list
 
 
@@ -162,24 +171,27 @@ if __name__ == "__main__":
     for frame in range(1, ce_constant['frame_total'], ce_constant['frame_interval']):
         shared_list = mp.Manager().list()
         for i in range(ce_constant['processes_nodes']):
-            p = mp.Process(target=coreFunction, args=(shared_list, history, core[i], frame,))
+            p = mp.Process(target=coreFunction, args=(shared_list, updated_list_of_values, core[i], frame,))
             p.start()
             processes_n.append(p)
         for p in processes_n:
             p.join()
+        
 
-        history = list(shared_list)
-        export_data_set.extend(history)
+        updated_list_of_values = list(shared_list)
+        
+        appendation = list(shared_list)
+        export_data_set.extend(appendation)
         print("done with frame ", frame)
         
-        #print(history) # test print
+        print(updated_list_of_values[3]) # test print
 
     #export_data_set = list(export_data_set)
-    print(export_data_set)
+    #print(export_data_set)
     #export_data_set = list(export_data_set.items())
     #print(export_data_set) # test print
     pd_dataframe = pd.DataFrame(export_data_set, columns=["frame", "name", "type", "mass", "radius", "location", "velocity"])
-    pd_dataframe.to_csv(r'N:\data3.csv')
+    pd_dataframe.to_csv(r'C:\data.csv')
     print("end1")
     print("end2")
 # breakpoint end   [set_frame, set_name, set_type, set_mass, set_visual_radius, set_location, set_velocity]
