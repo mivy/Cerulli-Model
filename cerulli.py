ef4eaaf59409afefc20db9732671670ee76f537a
mivy <grperlot@gmail.com>
2021-11-06 21:41:59 -0700

    Update cerulli.py

--- cerulli.py
+++ cerulli.py
@@ -13,8 +13,8 @@ const = {
     'frame_total' : 60,                 #
     'time' : 1,                         #
     'processes_nodes' : 10,             #
-    'samples' : 100,                    #
-    'mass_p_law' : -4.5,                #
+    'samples' : 10,                     #
+    'mass_p_law' : -4.0,                # -4.5 def
     'mass_scalar' : 100000,             #
     'location_scalar' : 3,              #
     'velocity_scalar' : 1,              #
@@ -52,8 +52,7 @@ def mass_distribution(): # truncated power law for m
     return t
 
 
-def anim_dataset(queue, history, core, frame): # columns=["frame", "name", "vx", "vy", "vz"] previous_set, anim_set, 
-    anim_set = queue.get()
+def anim_dataset(shared_list, history, core, frame): # columns=["frame", "name", "vx", "vy", "vz"] previous_set, anim_set, 
     for focus in range(core[0], core[1]): # total indicies counted here
         focus_obj = history[focus] # and call its values
         grav = [0,0,0]
@@ -68,36 +67,23 @@ def anim_dataset(queue, history, core, frame): # columns=["frame", "name", "vx",
             dx = target_obj[3] # location variables
             dy = target_obj[4]
             dz = target_obj[5]
-            dist = pythagorean(x, y, z, dx, dy, dz) 
-            grav = vector_gravity(dm, x, dx, y, dy, z, dz, dist, grav)
+            dist = math.hypot(dx - x, dy - y, dz - z)
+            acc_x = (-1)*((const['newton'])*dm*(x-dx))/(((dist**2)+(const['e']**2))**(3/2)) 
+            acc_y = (-1)*((const['newton'])*dm*(y-dy))/(((dist**2)+(const['e']**2))**(3/2)) 
+            acc_z = (-1)*((const['newton'])*dm*(z-dz))/(((dist**2)+(const['e']**2))**(3/2))
+            grav = [acc_x + grav[0], acc_y + grav[1], acc_z + grav[2]] 
         delta_x = grav[0]*const['time']**2+focus_obj[6]*const['time'] # new velocity
         delta_y = grav[1]*const['time']**2+focus_obj[7]*const['time'] # for export
         delta_z = grav[2]*const['time']**2+focus_obj[8]*const['time']
         coord_i = x + delta_x # new location
         coord_j = y + delta_y # for export
         coord_k = z + delta_z
-        set_return = [frame + 1, focus_obj[0], focus_obj[1], focus_obj[2], coord_i, coord_j, coord_k, delta_x, delta_y, delta_z] # queue adds frame
-        anim_set[focus] = set_return
-    queue.put(anim_set)
-
-
-def pythagorean(x, y, z, dx, dy, dz): # hypotenuse distance between objs' center
-    dist = math.hypot(dx - x, dy - y, dz - z) # working
-    return dist
-
-
-def vector_gravity(m, x, dx, y, dy, z, dz, dist, gravity): # newtonian gravity, while t = 1
-    acc_x = (-1)*((const['newton'])*m*(x-dx))/(((dist**2)+(const['e']**2))**(3/2)) 
-    acc_y = (-1)*((const['newton'])*m*(y-dy))/(((dist**2)+(const['e']**2))**(3/2)) 
-    acc_z = (-1)*((const['newton'])*m*(z-dz))/(((dist**2)+(const['e']**2))**(3/2)) 
-    gravity = [acc_x + gravity[0], acc_y + gravity[1], acc_z + gravity[2]]
-    return gravity
+        set_return = [frame + 1, focus_obj[0], focus_obj[1], focus_obj[2], coord_i, coord_j, coord_k, delta_x, delta_y, delta_z]
+        shared_list.append(set_return) # 
 
 
 if __name__ == "__main__":
     ret = {}
-    anim_set = {}
-    li = []
     init_l = []
     export_data = []
     sample_sum = const['processes_nodes'] * const['samples']
@@ -113,12 +99,15 @@ if __name__ == "__main__":
         p.join()
     ret = queue.get()
     init_l = list(ret.values())
-    for initn in range(sample_sum):
-        li.append([1, init_l[initn][0], init_l[initn][3], init_l[initn][4], init_l[initn][5]])
-    export_data.extend(li)
     pd_dataframe = pd.DataFrame(init_l, columns=["name", "mass", "radius", "lx", "ly", "lz", "vx", "vy", "vz"])
     pd_dataframe.to_csv(r'N:\init1.csv')
     print("done initializing ... [now rendering frames]") # print
+    anim_set = [] 
+    for i in range(len(init_l)): # make first frame anim_set
+        anim_set.append([1, init_l[i][0], init_l[i][3], init_l[i][4], init_l[i][5]])
+    print(anim_set[3])
+    print("done with frame 1")
+    export_data.extend(anim_set)
     core = []
     processes_n = []
     for i in range(const['processes_nodes']):
@@ -126,24 +115,25 @@ if __name__ == "__main__":
         k_process_sum = const['samples'] * (i + 1)
         core.append([i_process_sum, k_process_sum])
     for frame in range(1, const['frame_total'] + 1, const['frame_interval']):
-        queue.put(anim_set)
+        shared_list = mp.Manager().list()
         for i in range(const['processes_nodes']):
-            p = mp.Process(target=anim_dataset, args=(queue, init_l, core[i], frame,)) # previous_set, anim_set, 
+            p = mp.Process(target=anim_dataset, args=(shared_list, init_l, core[i], frame,)) # previous_set, anim_set, 
             p.start()
             processes_n.append(p)
         for p in processes_n:
             p.join()
-        anim_set = queue.get()
-        anim_list = list(ret.values())
-        export_list_temp = [] # building list to extend into animation dataset
-        for n in range(sample_sum): # ["frame", "name", "x", "y", "z"]
-            export_list_temp.append([anim_set[n][0], anim_set[n][1], anim_set[n][4], anim_set[n][5], anim_set[n][6]])
-        export_data.extend(export_list_temp)
-        return_list_temp = [] # building list for internal use in function
-        for m in range(sample_sum): # ["name", "mass", "radius", "lx", "ly", "lz", "vx", "vy", "vz"]
-            return_list_temp.append([anim_set[m][1], anim_set[m][2], anim_set[m][3], anim_set[m][4], anim_set[m][5], anim_set[m][6], anim_set[m][7], anim_set[m][8], anim_set[m][9]])
-        init_l = return_list_temp
-        print(init_l[3])
+        init_l = list(shared_list) # frame, name, mass, rad, x, y, z, delta_x, delta_y, delta_z]
+        anim_set = list(shared_list)
+        for i in init_l: # make new history list 
+            del i[0]
+        for i in anim_set: # make export list
+            del i[9]
+            del i[8]
+            del i[7]
+            del i[3]
+            del i[2]
+        print(anim_set[3])
+        export_data.extend(anim_set)
         print("done with frame", frame) # print
     pd_dataframe = pd.DataFrame(export_data, columns=["frame", "name", "x", "y", "z"])
     pd_dataframe.to_csv(r'N:\anim1.csv')
